f1416bc120f5026fc1d5104f5be3e3816be81f2a
Kalai <eie_kalai@yahoo.com>
2016-01-10 03:40:42 -0700

    gsdh

--- resources/views/kalai.blade.php
+++ resources/views/kalai.blade.php
@@ -13,26 +13,38 @@
     <link rel="stylesheet" type="text/css" href="css/home.css">
 </head>
 <body>
-
-<div class="container">
+{{--<div class="row ">--}}
+    {{--<div class="col-sm-12 jumbotron">--}}
+        {{--<h1>Bootstrap Tutorial</h1>--}}
+        {{--<p>Bootstrap is the most popular HTML, CSS, and JS framework for developing--}}
+            {{--responsive, mobile-first projects on the web.</p>--}}
+    {{--</div>--}}
+{{--</div>--}}
+<div class="container-fluid">
     <div class="row">
         <div class="col-sm-12 topbg"></div>
     </div>
-    <div class="row">
-        <div class="row bdy">
 
-                <div class="col-sm-8">
-                    <h1>Apt Man</h1>
-                    <h2>Your best apartment manager</h2>
-                </div>
-                <div class="col-sm-4">
-                    <button type="button" class="btn btn-success center-block">Sign In</button>
-                </div>
+    <div class="row bdy">
+        <div class="col-sm-8 ">
+            <h1 class="page-header">AptMan</h1>
+            <h2 class="well-sm">India's best apartment community manager</h2>
         </div>
-        <div class="row footer"></div>
+        <div class="col-sm-4">
+            <button type="button" class="btn btn-success center-block">Sign In</button>
+        </div>
     </div>
+    <div class="row">
+        <div class="col-sm-12">
+
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12 btmbg"></div>
+    </div>
+    <div class="row footer"></div>
 </div>
-    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
 </body>
 </html>
