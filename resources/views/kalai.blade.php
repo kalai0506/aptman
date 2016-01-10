0134a1e9f5e2e62dd5a1eeffdcf41cf03d5d18d7
Kalai <eie_kalai@yahoo.com>
2016-01-10 02:42:25 -0700

    gsdh

--- resources/views/kalai.blade.php
+++ resources/views/kalai.blade.php
@@ -7,42 +7,32 @@
     {{--<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">--}}
     {{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>--}}
     {{--<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>--}}
-    <!-- Bootstrap CSS served from a CDN -->
+            <!-- Bootstrap CSS served from a CDN -->
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css"
           rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/home.css">
 </head>
 <body>
 
-<div class="container-fluid">
+<div class="container">
     <div class="row">
         <div class="col-sm-12 topbg"></div>
-    <div class="row header">
-        <div class="col-sm-8">
-            <h1>Apt Man</h1>
-            <h2>Your best apartment manager</h2>
-        </div>
-        <div class="col-sm-4">
-            <button type="button" class="btn btn-success center-block">Sign In</button>
-        </div>
     </div>
-    <div class="row bdy">
-        <div class="col-sm-4"><button type="button" class="btn btn-default">Default</button></div>
-        <div class="col-sm-8">
-    <h2>Contextual Colors</h2>
-    <p>Use the contextual classes to provide "meaning through colors":</p>
-    <p class="text-muted">This text is muted.</p>
-    <p class="text-primary">This text is important.</p>
-    <p class="text-success">This text indicates success.</p>
-    <p class="text-info">This text represents some information.</p>
-    <p class="text-warning">This text represents a warning.</p>
-    <p class="text-danger">This text represents danger.</p>
-        </div>
+    <div class="row">
+        <div class="row bdy">
 
+                <div class="col-sm-8">
+                    <h1>Apt Man</h1>
+                    <h2>Your best apartment manager</h2>
+                </div>
+                <div class="col-sm-4">
+                    <button type="button" class="btn btn-success center-block">Sign In</button>
+                </div>
+        </div>
+        <div class="row footer"></div>
     </div>
-    <div class="row footer"></div>
 </div>
-<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
 </body>
 </html>
