7f5aa5efaeeb66e3535e4d7b144984270a9a56d2
Kalai <eie_kalai@yahoo.com>
2016-01-10 17:33:58 -0700

    wgc

--- resources/views/kalai.blade.php
+++ resources/views/kalai.blade.php
@@ -22,65 +22,65 @@
 {{--</div>--}}
 <div class="container-fluid">
     <div class="row">
-        <div class="col-lg-12 topbg"></div>
+        <div class="col-lg-12 col-sm-12 col-xs-12 topbg"></div>
     </div>
 
     <div class="row">
-        <div class="col-lg-8 logo">
+        <div class="col-lg-8 col-sm-8 col-xs-8 logo">
             <h1>aptman</h1>
             India's best apartment community manager
         </div>
-        <div class="col-lg-4">
+        <div class="col-lg-4 col-sm-4 col-xs-4">
             <button type="button" class="btn btn-success center-block">Sign In</button>
         </div>
     </div>
     <br><br><br><br><br>
     <div class="row">
-        <div class="col-lg-12 fillspace"></div>
+        <div class="col-lg-12 col-sm-12 col-xs-12 fillspace"></div>
     </div>
     <div class="row form-inline">
-        <div class="col-lg-3"></div>
-        <div class="col-lg-7">
+        <div class="col-lg-3 col-sm-3 col-xs-3"></div>
+        <div class="col-lg-7 col-sm-7 col-xs-7">
             <input type="text" class="form-control left" id="usr" size="55">
             <button type="button" class="btn btn-warning left">Search / Add your community</button>
         </div>
-        <div class="col-lg-2">
+        <div class="col-lg-2 col-sm-2 col-xs-2">
         </div>
     </div>
     <br><br>
     <div class="row">
-        <div class="col-lg-12 blankspace"></div>
+        <div class="col-lg-12 col-sm-12 col-xs-12 blankspace"></div>
     </div>
     <div class="row">
-        <div class="col-lg-12 btmbg"></div>
+        <div class="col-lg-12 col-sm-12 col-xs-12 btmbg"></div>
     </div>
     <div class="row footer">
         <br>
-        <div class="col-lg-3 ">aptman</div>
-        <div class="col-lg-3 ">Footer Col-2</div>
-        <div class="col-lg-3 ">Footer Col-3</div>
-        <div class="col-lg-3 ">Footer Col-4</div>
+        <div class="col-lg-3 col-sm-3 col-xs-3">aptman</div>
+        <div class="col-lg-3 col-sm-3 col-xs-3">Footer Col-2</div>
+        <div class="col-lg-3 col-sm-3 col-xs-3">Footer Col-3</div>
+        <div class="col-lg-3 col-sm-3 col-xs-3">Footer Col-4</div>
     </div>
     <div class="row subfooter">
         <br>
-        <div class="col-lg-3 ">Footer Col-1.1</div>
-        <div class="col-lg-3 ">Footer Col-2.1</div>
-        <div class="col-lg-3 ">Footer Col-3.1</div>
-        <div class="col-lg-3 ">Footer Col-4.1</div>
+        <div class="col-lg-3 col-sm-3 col-xs-3">Footer Col-1.1</div>
+        <div class="col-lg-3 col-sm-3 col-xs-3">Footer Col-2.1</div>
+        <div class="col-lg-3 col-sm-3 col-xs-3">Footer Col-3.1</div>
+        <div class="col-lg-3 col-sm-3 col-xs-3">Footer Col-4.1</div>
     </div>
     <div class="row subfooter">
         <br>
-        <div class="col-lg-3 ">Footer Col-1.2</div>
-        <div class="col-lg-3 ">Footer Col-2.2</div>
-        <div class="col-lg-3 ">Footer Col-3.2</div>
-        <div class="col-lg-3 ">Footer Col-4.2</div>
+        <div class="col-lg-3 col-sm-3 col-xs-3">Footer Col-1.2</div>
+        <div class="col-lg-3 col-sm-3 col-xs-3">Footer Col-2.2</div>
+        <div class="col-lg-3 col-sm-3 col-xs-3">Footer Col-3.2</div>
+        <div class="col-lg-3 col-sm-3 col-xs-3">Footer Col-4.2</div>
     </div>
     <div class="row subfooter">
         <br>
-        <div class="col-lg-3 ">Footer Col-1.3</div>
-        <div class="col-lg-3 ">Footer Col-2.3</div>
-        <div class="col-lg-3 ">Footer Col-3.3</div>
-        <div class="col-lg-3 ">Footer Col-4.3</div>
+        <div class="col-lg-3 col-sm-3 col-xs-3">Footer Col-1.3</div>
+        <div class="col-lg-3 col-sm-3 col-xs-3">Footer Col-2.3</div>
+        <div class="col-lg-3 col-sm-3 col-xs-3">Footer Col-3.3</div>
+        <div class="col-lg-3 col-sm-3 col-xs-3">Footer Col-4.3</div>
     </div>
 </div>
 <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
